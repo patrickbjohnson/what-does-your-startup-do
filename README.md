77eda41c88c08b5d6fbb8afb726c8ddc94ab027e
Patrick Johnson <pbj@Patricks-MacBook-Air.local>
2013-10-29 10:43:10 -0400

    Added final site stylings

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
-what-does-your-startup-do
+What Does Your Startup Do?
 =========================
+The site randomly selects a vague, nondescript 'mission statement' that outlines what a startup does. 
+
+We Don't know what they sell, what business or industry their in. Apparently we only care they sell 'happiness' and 'pixel-perfect' things. 
+
+
+Phrases
+-------------
+The site is randomly selecting from an array of objects. None of the phrases are used by any existing startups directly. Some have been modified from their original form. 
+
+- We connect users in single meeting place to share consumer experiences.
+- We create happy interactions for mobile audiences."
+- We’re a marketplace focused on positive relationships."
+- We’re a community of like minded individuals seeking wonderful memories."
+- We empower local business to expand their local commerce efforts globally."
+- We enable people everywhere to make money doing what they love."
+- We help bring communities closer together."
+- We seek out and support artisans, experts and entrepreneurs who love their craft."
+- We provide consumers with online solutions that are innovative, easy to use, and work for everyday web users."
+- We love connecting your brand to empowered consumers through an integrated strategy optimized for the digital space.
+- We provide integrated technology to Internet-based companies.
+- We discover and share awesome technology that makes a difference.
+- We share news on the greatest apps and devices that matter.
+- We help you find and share the things that matter most.
+- We generate buzz and brand-awareness for consumers.
+- We handcraft brand experiences to enable brand advocates & consumers.
+
+If you'd like to contribute by addigin additional phrases [email me](mailto:im@pbj.me).
--- js/main.js
+++ js/main.js
@@ -1,11 +1,35 @@
 $(document).ready(function(){
-	var bullshit = ["test1", "test2", "test3", "test4", "test5", "test6", "test7", "test8", "test8", "test9", "test10", "test11", "test12", "test13", "test14", "test15", "test16", "test17"];
-	console.log(bullshit[0], bullshit[1]);
 
-	var number = bullshit[Math.floor(Math.random() * bullshit.length)];
-	console.log(number);
+	var bullshit = [
+			{"phrase": "We connect users in single meeting place to share consumer experiences."},
+			{"phrase": "We create happy interactions for mobile audiences." },
+			{"phrase": "We’re a marketplace focused on positive relationships." },
+			{"phrase": "We’re a community of like minded individuals seeking wonderful memories." },
+			{"phrase": "We empower local business to expand their local commerce efforts globally." },
+			{"phrase": "We enable people everywhere to make money doing what they love." },
+			{"phrase": "We help bring communities closer together." },
+			{"phrase": "We seek out and support artisans, experts and entrepreneurs who love their craft." },
+			{"phrase": "We provide consumers with online solutions that are innovative, easy to use, and work for everyday web users." },
+			{"phrase": "We love connecting your brand to empowered consumers through an integrated strategy optimized for the digital space."},
+			{"phrase": "We provide integrated technology to Internet-based companies."},
+			{"phrase": "We discover and share awesome technology that makes a difference"},
+			{"phrase": "We share news on the greatest apps and devices that matter."},
+			{"phrase": "We help you find and share the things that matter most."},
+			{"phrase": "We generate buzz and brand-awareness for consumers."},
+			{"phrase": "We handcraft brand experiences to enable brand advocates & consumers."}
+			];
 
-	$('.main .bullshit').text(number);
+	random();
+
+	function random(){
+		var number = bullshit[Math.floor(Math.random() * bullshit.length)];
+		$('.main .bullshit').text(number.phrase).animate({});
+	};
+
+	$('.main button').on('click', function(){
+		random();
+	});
+
 });
 
 
